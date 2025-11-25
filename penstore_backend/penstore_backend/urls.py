7b29301cd4e0ac9abf91e91c0888f8c43fae0f58
vencce <vitorferreira20005@gmial.com>
2025-11-25 17:27:11 -0300

    voltando para o padrao

--- penstore_backend/penstore_backend/urls.py
+++ penstore_backend/penstore_backend/urls.py
@@ -1,17 +1,19 @@
+# penstore_backend/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='admin/', permanent=True)),
     path('admin/', admin.site.urls),
     
+    # APIs da nossa Loja (E-commerce):
     path('api/', include('loja.urls')), 
     
+    # APIs do novo sistema de Fábrica (ERP):
     path('api/fabrica/', include('fabrica.urls')), 
 ]
 
+# Adiciona as rotas de mídia em modo de desenvolvimento
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
